b60c5fdc9c23630436a2ecfa7c6ac421de080773
SJMosley <samueljmosley@gmail.com>
2025-05-07 12:43:22 -0500

    working state, prep for rewrite of widget creations

--- src/main.py
+++ src/main.py
@@ -1,37 +1,92 @@
 from conversion_helper import *
-# from ui.graphics import Window
+from pottery_calc_helper import *
+from functools import partial
+from ui.graphics import Input
 import tkinter as tk
+
+# def get_text():
+#     text = entry.get()
+#     print("You entered:", text)
+
+# def get_num():
+#     my_num = float(entry.get())
+#     print("You entered:", my_num)
+
+inputs = []
 
 def setup_window(window):
     window.title("Pottery Calculator")
     main_frame = tk.Frame()
-    main_frame.pack()
+
     #Desired Size Input items
-    # desired_frame = tk.Frame()
-    # desired_frame.pack()
-    # thrown_frame = tk.Frame()
-    # thrown_frame.pack()
+    desired_frame = tk.Frame(master=window, width=40)
+
+    #
+    thrown_frame = tk.Frame()
+    build_desired_frame(desired_frame)
+    build_thrown_frame(thrown_frame)
+
+    #Add Calculate Button
+
+
+    button = tk.Button(master=desired_frame, text="Calculate Thrown!", command=partial(calculate_thrown, (desired_frame, thrown_frame)))
+    # button.bind("<Button-1>", calculate_thrown) #button 1 means left click
+    button.pack()
+    desired_frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+    thrown_frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+    main_frame.pack()
+    window.mainloop()
+def build_desired_frame(desired_frame):
     ui_items = []
-    label_diameter = tk.Label(text="Desired Inner Diameter")
-    entry_diameter = tk.Entry()
-    ui_items.append((label_diameter, entry_diameter))
-    label_height = tk.Label(text="Desired Inner Height")
-    entry_height = tk.Entry()
-    ui_items.append((label_height, entry_height))
-    label_shrinkage = tk.Label(text="Shrinkage")
-    entry_shrinkage = tk.Entry()
-    ui_items.append((label_shrinkage, entry_shrinkage))
-    label_thickness = tk.Label(text="Wall Thickness")
-    entry_thickness = tk.Entry()
-    ui_items.append((label_thickness, entry_thickness))
+    lbl_diameter = tk.Label(master=desired_frame, text="Desired Inner Diameter")
+    ent_diameter = tk.Entry(master=desired_frame, width=40)
+    ui_items.append((lbl_diameter, ent_diameter))
+    lbl_height = tk.Label(master=desired_frame, text="Desired Inner Height")
+    ent_height = tk.Entry(master=desired_frame, width=40)
+    ui_items.append((lbl_height, ent_height))
+    lbl_shrinkage = tk.Label(master=desired_frame, text="Shrinkage")
+    ent_shrinkage = tk.Entry(master=desired_frame, width=40)
+    ent_shrinkage.insert(0,"0.12")
+    ui_items.append((lbl_shrinkage, ent_shrinkage))
+    lbl_thickness = tk.Label(master=desired_frame, text="Wall Thickness")
+    ent_thickness = tk.Entry(master=desired_frame, width=40)
+    ent_thickness.insert(0, "0.25")
+    ui_items.append((lbl_thickness, ent_thickness))
 
     for item in ui_items:
         item[0].pack()
         item[1].pack()
+def build_thrown_frame(thrown_frame):
+    ui_items = []
 
-    #Add Calculate Button
+    lbl_diameter = tk.Label(master=thrown_frame, text="Thrown Diameter")
+    ent_diameter = tk.Entry(master=thrown_frame, width=40)
+    ui_items.append((lbl_diameter, ent_diameter))
+    lbl_height = tk.Label(master=thrown_frame, text="Thrown Inner Height")
+    ent_height = tk.Entry(master=thrown_frame, width=40)
+    ui_items.append((lbl_height, ent_height))
+    lbl_thickness = tk.Label(master=thrown_frame, text="Thrown Wall Thickness")
+    ent_thickness = tk.Entry(master=thrown_frame, width=40)
+    # ent_thickness.insert(0, "0.25")
+    ui_items.append((lbl_thickness, ent_thickness))
+    for item in ui_items:
+        item[0].pack()
+        item[1].pack()
 
-    window.mainloop()
+def calculate_thrown(frame_tuple):
+    print("Calculating!")
+    desired_frame = frame_tuple[0]
+    thrown_frame = frame_tuple[1]
+
+    # for widget in desired_frame.winfo_children():
+    #     if isinstance(widget, tk.Entry)
+    # #shrinkage, d_inner_diam, d_inner_height, d_wall_thickness, d_base_thickness
+    # shrinkage =
+    # d_i_diam =
+    # d_i_height =
+    # wall_thickness =
+    # d_base_thickness =
+    # inner_inch_dimensions_to_all_values
 
 def main():
     window = tk.Tk()
--- src/ui/graphics.py
+++ src/ui/graphics.py
@@ -1,14 +1,19 @@
-from tkinter import Tk, Canvas
+import tkinter as tk
 
 # x=0 is left of screen
 # y=0 is top of screen
 # 0,0 is top left of screen
 class Window:
     def __init__(self, width, height):
-        self.__root = Tk()
+        self.__root = tk.
         self.__root.title("Pottery Calculator")
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def close(self):
         self.__running = False
+class Input:
+    def __init__(self, name, frame = None, width = 40):
+        self.__name = name
+        self.label = tk.Label(master=frame, text=name)
+        self.entry = tk.Entry(master=frame, width=width)
